63875d5c9cfba37a8d313d0db795b67b4d0e5fb7
DaPorkchop_ <daporkchop@daporkchop.net>
2020-12-27 10:42:43 +0100

    add JVM flag to include stack trace in OutOfProjectionBoundsException

--- src/main/java/io/github/terra121/projection/OutOfProjectionBoundsException.java
+++ src/main/java/io/github/terra121/projection/OutOfProjectionBoundsException.java
@@ -1,9 +1,19 @@
 package io.github.terra121.projection;
 
 public final class OutOfProjectionBoundsException extends Exception {
-    public static final OutOfProjectionBoundsException INSTANCE = new OutOfProjectionBoundsException();
+    private static final OutOfProjectionBoundsException INSTANCE = new OutOfProjectionBoundsException(false);
 
-    private OutOfProjectionBoundsException() {
-        super(null, null, false, false);
+    private static final boolean FAST = Boolean.parseBoolean(System.getProperty("terraplusplus.fastExcept", "true"));
+
+    public static OutOfProjectionBoundsException get() {
+        if (FAST) {
+            return INSTANCE;
+        } else {
+            return new OutOfProjectionBoundsException(true);
+        }
+    }
+
+    private OutOfProjectionBoundsException(boolean flag) {
+        super(null, null, flag, flag);
     }
 }
