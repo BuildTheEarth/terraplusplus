696a6ab63b309daec5bc74d798a7b8baecfb1d6f
DaPorkchop_ <daporkchop@daporkchop.net>
2020-12-28 15:12:05 +0100

    workaround broken bathymetry data at high resolution
    see https://github.com/tilezen/joerd/issues/199

--- src/main/java/io/github/terra121/control/TerraTeleport.java
+++ src/main/java/io/github/terra121/control/TerraTeleport.java
@@ -157,6 +157,4 @@ public class TerraTeleport extends Command {
         }
         return sender.canUseCommand(2, "");
     }
-
-
 }
--- src/main/java/io/github/terra121/dataset/impl/Heights.java
+++ src/main/java/io/github/terra121/dataset/impl/Heights.java
@@ -29,35 +29,70 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+
+import static net.daporkchop.lib.common.math.PMath.*;
+import static net.daporkchop.lib.common.util.PValidation.*;
 
 public class Heights extends DoubleTiledDataset {
     public static void main(String... args) throws OutOfProjectionBoundsException {
-        Http.setMaximumConcurrentRequestsTo("https://s3.amazonaws.com/", 16);
+        Http.configChanged();
+        ScalarDataset dataset = constructDataset(BlendMode.LINEAR);
 
-        int size = 512;
-        int shift = 0;
-        ScalarDataset heights = Heights.constructDataset(BlendMode.LINEAR);
-        BufferedImage img = new BufferedImage(size << shift, size << shift, BufferedImage.TYPE_INT_ARGB);
+        int sizeX = 1024;
+        int sizeZ = 1024;
+        double baseX = 7;
+        double baseZ = 0;
+        double r = 1;
+        PorkUtil.simpleDisplayImage(true, get(dataset, sizeX, sizeZ, Bounds2d.of(baseX - r, baseX + r, baseZ - r, baseZ + r), 16)
+                .thenApply(data -> {
+                    BufferedImage img = new BufferedImage(sizeX, sizeZ, BufferedImage.TYPE_INT_ARGB);
+                    double min = Arrays.stream(data).min().getAsDouble();
+                    double max = Arrays.stream(data).max().getAsDouble();
 
-        int[] arr = new int[1 << shift];
+                    System.out.printf("min: %f, max: %f\n", min, max);
 
-        Bounds2d bounds = Bounds2d.of(15.1, 15.4, 46.1, 46.4);
-        double[] data = heights
-                .getAsync(bounds.toCornerBB(new EquirectangularProjection(), true), size, size)
-                .join();
+                    for (int x = 0; x < sizeX; x++) {
+                        for (int z = 0; z < sizeZ; z++) {
+                            int h = clamp(floorI((data[x * sizeZ + z] - min) * 255.0d / (max - min)), 0, 255);
+                            img.setRGB(x, z, 0xFF000000 | h << 16 | h << 8 | h);
+                        }
+                    }
 
-        double min = Arrays.stream(data).min().orElse(0.0d);
-        double max = Arrays.stream(data).max().orElse(1.0d);
-        for (int x = 0; x < size; x++) {
-            for (int z = 0; z < size; z++) {
-                double d = data[x * size + z];
-                int c = PMath.floorI((d - min) / (max - min) * 255.0d);
-                Arrays.fill(arr, 0xFF000000 | c << 16 | c << 8 | c);
-                img.setRGB(x << shift, z << shift, 1 << shift, 1 << shift, arr, 0, 0);
+                    return img;
+                })
+                .join());
+    }
+
+    private static CompletableFuture<double[]> get(ScalarDataset dataset, int sizeX, int sizeZ, Bounds2d bb, int parallelization) {
+        CompletableFuture[] futures = new CompletableFuture[positive(parallelization, "parallelization")];
+        CompletableFuture start = CompletableFuture.completedFuture(null);
+        double[] dst = new double[sizeX * sizeZ];
+        for (int thread = 0; thread < parallelization; thread++) {
+            int minX = (sizeX / parallelization) * thread;
+            int maxX = (sizeX / parallelization) * (thread + 1);
+            CompletableFuture future = start;
+            for (int _z = 0; _z < sizeZ; _z++) {
+                for (int _x = minX; _x < maxX; _x++) {
+                    int x = _x;
+                    int z = _z;
+                    double lon = lerp(bb.minX(), bb.maxX(), x / (double) sizeX);
+                    double lat = lerp(bb.minZ(), bb.maxZ(), z / (double) sizeZ);
+                    future = future.thenCompose(unused -> {
+                        try {
+                            return dataset.getAsync(lon, lat).thenAccept(v -> {
+                                checkState(v != null && !Double.isNaN(v), v);
+                                dst[x * sizeZ + z] = v;
+                            });
+                        } catch (OutOfProjectionBoundsException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
             }
+            futures[thread] = future;
         }
-
-        PorkUtil.simpleDisplayImage(true, img);
+        return CompletableFuture.allOf(futures).thenApply(unused -> dst);
     }
 
     public static ScalarDataset constructDataset(@NonNull BlendMode blend) {
