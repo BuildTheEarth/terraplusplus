6bd44b752ceaa50da65d28b796cd18e44502f715
DaPorkchop_ <daporkchop@daporkchop.net>
2021-01-19 18:52:46 +0100

    make debug visualizer a lot fancier

--- src/main/java/io/github/terra121/generator/GenTest.java
+++ src/main/java/io/github/terra121/generator/GenTest.java
@@ -1,6 +1,10 @@
 package io.github.terra121.generator;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.LoadingCache;
 import io.github.terra121.BTEWorldType;
+import io.github.terra121.generator.cache.CachedChunkData;
 import io.github.terra121.generator.cache.ChunkDataLoader;
 import io.github.terra121.projection.OutOfProjectionBoundsException;
 import io.github.terra121.util.http.Http;
@@ -10,22 +14,31 @@ import net.minecraft.init.Blocks;
 import net.minecraft.init.Bootstrap;
 import net.minecraft.util.math.ChunkPos;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 import static net.daporkchop.lib.common.math.PMath.*;
 
 //TODO: delete this before merge
 public class GenTest {
-    static final int SIZE = 1024;
+    static final int SIZE = 512;
     static int BASE_CHUNK_X = 0;
     static int BASE_CHUNK_Z = 0;
-    static final int SCALE = 1;
+    static int SCALE = -1;
 
     static final int CHUNKS = SIZE >> 4;
 
-    static ChunkDataLoader LOADER;
+    static Function<ChunkPos, CompletableFuture<CachedChunkData>> GET_FUNC;
 
     public static void main(String... args) throws OutOfProjectionBoundsException {
         Bootstrap.register();
@@ -39,61 +52,125 @@ public class GenTest {
     private static void doThing() throws OutOfProjectionBoundsException { //allows hot-swapping
         EarthGeneratorSettings cfg = new EarthGeneratorSettings("{\"projection\":\"equirectangular\",\"orentation\":\"upright\",\"scaleX\":100000.0,\"scaleY\":100000.0,\"smoothblend\":true,\"roads\":false,\"customcubic\":\"\",\"dynamicbaseheight\":true,\"osmwater\":true,\"buildings\":false,\"caves\":false,\"lidar\":false,\"customdataset\":\"Custom Terrain Directory\"}");
         cfg = new EarthGeneratorSettings(BTEWorldType.BTE_GENERATOR_SETTINGS);
-        LOADER = new ChunkDataLoader(new GeneratorDatasets(cfg.getProjection(), cfg, true));
 
         double[] proj = cfg.getProjection().fromGeo(8.57696, 47.21763);
-        BASE_CHUNK_X = (floorI(proj[0]) >> 4) - (CHUNKS >> 1);
-        BASE_CHUNK_Z = (floorI(proj[1]) >> 4) - (CHUNKS >> 1);
+        BASE_CHUNK_X = floorI(proj[0]) >> 4;
+        BASE_CHUNK_Z = floorI(proj[1]) >> 4;
 
-        PorkUtil.simpleDisplayImage(true, tile(0, 0, SCALE)
-                .thenApply(data -> {
-                    System.out.println(Arrays.toString(proj));
+        LoadingCache<ChunkPos, CompletableFuture<CachedChunkData>> cache = CacheBuilder.newBuilder()
+                .softValues()
+                .build(new ChunkDataLoader(new GeneratorDatasets(cfg.getProjection(), cfg, true)));
+        GET_FUNC = cache::getUnchecked;
 
-                    BufferedImage img = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_ARGB);
+        JFrame frame = new JFrame();
+        BufferedImage img = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_ARGB);
+        ImageIcon icon = new ImageIcon(img);
+        JLabel label = new JLabel(icon);
+        frame.getContentPane().add(label);
 
-                    double minH = Arrays.stream(data[0]).min().getAsDouble();
-                    double maxH = Arrays.stream(data[0]).max().getAsDouble();
-                    double minW = Arrays.stream(data[1]).min().getAsDouble();
-                    double maxW = Arrays.stream(data[1]).max().getAsDouble();
-                    double minB = Arrays.stream(data[2]).min().getAsDouble();
-                    double maxB = Arrays.stream(data[2]).max().getAsDouble();
+        Runnable updateImage = () -> {
+            int off = floorI((CHUNKS >> 1) * Math.pow(2.0d, SCALE));
+            BASE_CHUNK_X -= off;
+            BASE_CHUNK_Z -= off;
+            tile(0, 0, SCALE)
+                    .thenApply(data -> {
+                        double minH = Arrays.stream(data[0]).min().getAsDouble();
+                        double maxH = Arrays.stream(data[0]).max().getAsDouble();
+                        double minW = Arrays.stream(data[1]).min().getAsDouble();
+                        double maxW = Arrays.stream(data[1]).max().getAsDouble();
+                        double minB = Arrays.stream(data[2]).min().getAsDouble();
+                        double maxB = Arrays.stream(data[2]).max().getAsDouble();
 
-                    System.out.printf("H: %.4f %.4f\n", minH, maxH);
-                    System.out.printf("W: %.4f %.4f\n", minW, maxW);
-                    System.out.printf("B: %.4f %.4f\n", minB, maxB);
+                        /*System.out.printf("H: %.4f %.4f\n", minH, maxH);
+                        System.out.printf("W: %.4f %.4f\n", minW, maxW);
+                        System.out.printf("B: %.4f %.4f\n", minB, maxB);*/
 
-                    for (int x = 0; x < SIZE; x++) {
-                        for (int z = 0; z < SIZE; z++) {
-                            int h = clamp(floorI((data[0][x * SIZE + z] - minH) * 255.0d / (maxH - minH)), 0, 255);
-                            int w = clamp(floorI((data[1][x * SIZE + z] - minW) * 255.0d / (maxW - minW)), 0, 255);
-                            int b = clamp(floorI((data[2][x * SIZE + z] - minB) * 255.0d / (maxB - minB)), 0, 255);
+                        for (int x = 0; x < SIZE; x++) {
+                            for (int z = 0; z < SIZE; z++) {
+                                int h = clamp(floorI((data[0][x * SIZE + z] - minH) * 255.0d / (maxH - minH)), 0, 255);
+                                int w = clamp(floorI((data[1][x * SIZE + z] - minW) * 255.0d / (maxW - minW)), 0, 255);
+                                int b = clamp(floorI((data[2][x * SIZE + z] - minB) * 255.0d / (maxB - minB)), 0, 255);
 
-                            img.setRGB(x, z, 0xFF000000 | h << 16 | w | b << 8);
+                                img.setRGB(x, z, 0xFF000000 | h << 16 | w | b << 8);
+                            }
                         }
-                    }
-                    return img;
-                })
-                .join());
+
+                        icon.getImage().flush();
+                        label.repaint();
+                        return img;
+                    })
+                    .join();
+            BASE_CHUNK_X += off;
+            BASE_CHUNK_Z += off;
+        };
+
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_RIGHT:
+                        BASE_CHUNK_X += CHUNKS >> 2;
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        BASE_CHUNK_X -= CHUNKS >> 2;
+                        break;
+                    case KeyEvent.VK_UP:
+                        BASE_CHUNK_Z -= CHUNKS >> 2;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        BASE_CHUNK_Z += CHUNKS >> 2;
+                        break;
+                    case KeyEvent.VK_ADD:
+                        SCALE--;
+                        break;
+                    case KeyEvent.VK_SUBTRACT:
+                        SCALE++;
+                        break;
+                }
+                updateImage.run();
+            }
+        });
+
+        updateImage.run();
+        frame.pack();
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        CompletableFuture<Void> f = new CompletableFuture<>();
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                f.complete(null);
+            }
+        });
+        f.join();
     }
 
     static CompletableFuture<double[][]> tile(int tileX, int tileZ, int level) {
         double[][] dst = new double[3][SIZE * SIZE];
         CompletableFuture[] futures;
-        if (level == 0) {
-            futures = new CompletableFuture[CHUNKS * CHUNKS];
-            for (int i = 0, chunkX = 0; chunkX < CHUNKS; chunkX++) {
-                for (int chunkZ = 0; chunkZ < CHUNKS; chunkZ++) {
+        if (level <= 0) {
+            int d = 1 << -level;
+            int s = -level;
+            futures = new CompletableFuture[(CHUNKS >> -level) * (CHUNKS >> -level)];
+            for (int i = 0, chunkX = 0; chunkX < CHUNKS >> -level; chunkX++) {
+                for (int chunkZ = 0; chunkZ < CHUNKS >> -level; chunkZ++) {
                     int offX = chunkX << 4;
                     int offZ = chunkZ << 4;
-                    futures[i++] = LOADER.load(new ChunkPos(BASE_CHUNK_X + chunkX + tileX * CHUNKS, BASE_CHUNK_Z + chunkZ + tileZ * CHUNKS))
+                    futures[i++] = GET_FUNC.apply(new ChunkPos(BASE_CHUNK_X + chunkX + tileX * CHUNKS, BASE_CHUNK_Z + chunkZ + tileZ * CHUNKS))
                             .thenAccept(data -> {
                                 for (int x = 0; x < 16; x++) {
                                     for (int z = 0; z < 16; z++) {
-                                        int j = (offX + x) * SIZE + offZ + z;
-                                        dst[0][j] = data.groundHeight(x, z);
-                                        dst[1][j] = data.surfaceHeight(x, z) - data.groundHeight(x, z);
-                                        IBlockState state = data.surfaceBlocks().get(x * 16 + z);
-                                        dst[2][j] = state == null ? 0.0d : state.getBlock() == Blocks.CONCRETE ? 1.0d : 2.0d;
+                                        for (int dx = 0; dx < d; dx++) {
+                                            for (int dz = 0; dz < d; dz++) {
+                                                int j = (((offX + x) << s) + dx) * SIZE + ((offZ + z) << s) + dz;
+                                                dst[0][j] = data.groundHeight(x, z);
+                                                dst[1][j] = data.surfaceHeight(x, z) - data.groundHeight(x, z);
+                                                IBlockState state = data.surfaceBlocks().get(x * 16 + z);
+                                                dst[2][j] = state == null ? 0.0d : state.getBlock() == Blocks.CONCRETE ? 1.0d : 2.0d;
+                                            }
+                                        }
                                     }
                                 }
                             });
