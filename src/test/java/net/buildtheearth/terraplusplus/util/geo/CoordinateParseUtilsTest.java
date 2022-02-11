ae86e4c0e07ec8b4c0ee907c508eeaffe7a23935
SmylerMC <smyler@mail.com>
2022-02-11 02:23:06 +0100

    Parse coordinates that use a comma as the decimal separator

--- src/test/java/net/buildtheearth/terraplusplus/util/geo/CoordinateParseUtilsTest.java
+++ src/test/java/net/buildtheearth/terraplusplus/util/geo/CoordinateParseUtilsTest.java
@@ -7,9 +7,9 @@ import org.junit.Test;
  * @author SmylerMC
  */
 public class CoordinateParseUtilsTest {
-    
+
     private static final double PRECISION = 1e-7;
-    
+
     @Test
     public void test() {
         this.testValidStringParsing("02°49'52\"N131°47'03\"E", 131.784167, 2.831111);
@@ -19,23 +19,42 @@ public class CoordinateParseUtilsTest {
         this.testValidStringParsing("02°49'52\"S 131°47'03\"E", 131.784167, -2.831111);
         this.testValidStringParsing("02°49'52\"N 131°47'03\"W", -131.784167, 2.831111);
         this.testValidStringParsing("02°49'52\"s 131°47'03\"w", -131.784167, -2.831111);
+
         this.testValidStringParsing("2.831111s 131.784167w", -131.784167, -2.831111);
         this.testValidStringParsing("-2.831111 -131.784167", -131.784167, -2.831111);
-        
+        this.testValidStringParsing("2,831111s 131,784167w", -131.784167, -2.831111);
+        this.testValidStringParsing("-2,831111 -131,784167", -131.784167, -2.831111);
+
         this.testValidStringParsing("02°49'52\"N;131°47'03\"E", 131.784167, 2.831111);
         this.testValidStringParsing("2.831111s;131.784167w", -131.784167, -2.831111);
         this.testValidStringParsing("-2.831111;-131.784167", -131.784167, -2.831111);
         this.testValidStringParsing("02°49'52\"N,131°47'03\"E", 131.784167, 2.831111);
         this.testValidStringParsing("2.831111s,131.784167w", -131.784167, -2.831111);
         this.testValidStringParsing("-2.831111,-131.784167", -131.784167, -2.831111);
-        
+
+        // Comma variant of the above
+        this.testValidStringParsing("2,831111s;131,784167w", -131.784167, -2.831111);
+        this.testValidStringParsing("-2,831111;-131,784167", -131.784167, -2.831111);
+        this.testValidStringParsing("2,831111s 131,784167w", -131.784167, -2.831111);
+        this.testValidStringParsing("-2,831111 -131,784167", -131.784167, -2.831111);
+
         this.testValidStringParsing("2 131", 131, 2);
         this.testValidStringParsing("-2 -131", -131, -2);
         this.testValidStringParsing("0. 0.", 0, 0);
         this.testValidStringParsing("0 0", 0, 0);
         this.testValidStringParsing("0.1 0", 0, 0.1);
+
+        // Comma variant of the above
+        this.testValidStringParsing("2 131", 131, 2);
+        this.testValidStringParsing("-2 -131", -131, -2);
+        this.testValidStringParsing("0, 0,", 0, 0);
+        this.testValidStringParsing("0 0", 0, 0);
+        this.testValidStringParsing("0,1 0", 0, 0.1);
+
+        // More than one space on the middle
+        this.testValidStringParsing("0,1  0", 0, 0.1);
     }
-    
+
     private void testValidStringParsing(String string, double longitude, double latitude) {
         LatLng latLng = CoordinateParseUtils.parseVerbatimCoordinates(string);
         Assert.assertNotNull(String.format("Failed to parse a valid coordinate string: %s", string), latLng);
