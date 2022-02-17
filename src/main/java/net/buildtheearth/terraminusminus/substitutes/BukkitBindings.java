5907790da301ad6890a2f488c3a0ca81e84a1ba0
SmylerMC <smyler@mail.com>
2022-02-17 10:54:02 +0100

    Be more liberal with block states -> bukkit

--- src/main/java/net/buildtheearth/terraminusminus/substitutes/BukkitBindings.java
+++ src/main/java/net/buildtheearth/terraminusminus/substitutes/BukkitBindings.java
@@ -6,34 +6,34 @@ import org.bukkit.block.data.BlockData;
 import java.util.stream.Collectors;
 
 /**
- * Makes life easier when working with Bukkit and Terra--
+ * Makes life easier when working with Bukkit and Terra--.
  *
  * @author SmylerMC
  */
-public class BukkitBindings {
+public final class BukkitBindings {
 
-    public static BlockData getAsBlockData(BlockState state, boolean legacy) {
+    public static BlockData getAsBlockData(BlockState state) {
         if (state == null) return null;
         BlockStateBuilder.BlockStateImplementation implementation = null;
         if (state instanceof BlockStateBuilder.BlockStateImplementation imp) {
             if (imp.bukkitBlockData != null) return (BlockData) imp.bukkitBlockData;
             implementation = imp;
         }
-        Material material = Material.getMaterial(state.getBlock().toString(), legacy);
+        Material material = Material.matchMaterial(state.getBlock().toString());
         if (material == null) return null;
         BlockData data = material.createBlockData(getPropertiesString(state));
         if (implementation != null) implementation.bukkitBlockData = data;
         return data;
     }
 
-    public static BlockData getAsBlockData(BlockState state) {
-        return getAsBlockData(state, false);
-    }
-
     private static String getPropertiesString(BlockState state) {
         return "[" + state.getProperties().entrySet().stream().map(
                     entry -> entry.getKey() + "=" + entry.getValue().getAsString()
                 ).collect(Collectors.joining(",")) + ']';
+    }
+
+    private BukkitBindings() {
+        throw new IllegalStateException();
     }
 
 }
