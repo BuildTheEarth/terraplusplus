af690231980541dee7aa1fb886dbc64f6e81bee5
Smyler <smyler@mail.com>
2025-08-14 16:36:56 +0200

    Load Terra-- dynamically from Gradle generated resource

--- src/main/java/net/buildtheearth/terraminusminus/TerraConstants.java
+++ src/main/java/net/buildtheearth/terraminusminus/TerraConstants.java
@@ -11,9 +11,35 @@ import net.buildtheearth.terraminusminus.substitutes.BlockState;
 import net.buildtheearth.terraminusminus.substitutes.Biome;
 import net.buildtheearth.terraminusminus.util.BiomeDeserializeMixin;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static com.google.common.base.Strings.*;
+
 public class TerraConstants {
     public static final String LIB_NAME = "terraminusminus";
-    public static final String LIB_VERSION = "(development_snapshot)";
+    public static final String LIB_VERSION;
+
+    static {
+        String libVersion = "unknown";
+        try (InputStream inputStream = TerraConstants.class.getResourceAsStream("terraminusminus.properties")) {
+            if (inputStream == null) {
+                throw new IOException("Terraminusminus properties file not found");
+            }
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            String version = properties.getProperty("version");
+            if (isNullOrEmpty(version)) {
+                throw new IOException("version property is missing from terraminusminus.properties (or is empty)");
+            }
+            libVersion = version;
+        } catch (IOException e) {
+            TerraMinusMinus.LOGGER.error("Failed to load Terraminusminus properties file, version will be unknown");
+            TerraMinusMinus.LOGGER.catching(e);
+        }
+        LIB_VERSION = libVersion;
+    }
 
     public static final Gson GSON = new GsonBuilder()
     		.registerTypeAdapter(BlockState.class, BlockStateParser.INSTANCE)
