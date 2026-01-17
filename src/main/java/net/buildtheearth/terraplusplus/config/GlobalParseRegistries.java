SmylerMC/terraminusminus

Bump Apache commons imaging to 1.0-alpha3
We no longer need to use a custom patched version of the lib as 1.0-alpha3 has support for integer tiff and was released almost 4 years ago.

src/main/java/net/buildtheearth/terraminusminus/config/GlobalParseRegistries.java
@@ -20,6 +20,7 @@ import net.buildtheearth.terraminusminus.config.scalarparse.d.FlipZDSP;
 import net.buildtheearth.terraminusminus.config.scalarparse.d.FromIntDSP;
 import net.buildtheearth.terraminusminus.config.scalarparse.d.MultiplyDSP;
 import net.buildtheearth.terraminusminus.config.scalarparse.d.ParseTiffAutoDSP;
+import net.buildtheearth.terraminusminus.config.scalarparse.d.ParseTiffDSP;
 import net.buildtheearth.terraminusminus.config.scalarparse.d.ParseTiffFloatingPointDSP;
 import net.buildtheearth.terraminusminus.config.scalarparse.d.ParseTerrariumPngDSP;
 import net.buildtheearth.terraminusminus.config.scalarparse.d.ParseTiffIntDSP;
@@ -111,8 +112,9 @@ public class GlobalParseRegistries {
             .put("flip_z", FlipZDSP.class)
             .put("from_int", FromIntDSP.class)
             .put("swap_axes", SwapAxesDSP.class)
-            //parse operators
+            //parse operators (including deprecated ParseTiffXXXXDSPs)
             .put("parse_png_terrarium", ParseTerrariumPngDSP.class)
+            .put("parse_tiff", ParseTiffDSP.class)
             .put("parse_tiff_auto", ParseTiffAutoDSP.class)
             .put("parse_tiff_fp", ParseTiffFloatingPointDSP.class)
             .put("parse_tiff_int", ParseTiffIntDSP.class)

src/main/java/net/buildtheearth/terraminusminus/config/scalarparse/d/ParseTiffDSP.java
@@ -1,0 +1,115 @@
+package net.buildtheearth.terraminusminus.config.scalarparse.d;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import org.apache.commons.imaging.FormatCompliance;
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.common.bytesource.ByteSourceInputStream;
+import org.apache.commons.imaging.formats.tiff.*;
+import org.apache.commons.imaging.formats.tiff.constants.GdalLibraryTagConstants;
+
+import java.io.IOException;
+
+import static net.daporkchop.lib.common.util.PValidation.checkArg;
+import static org.apache.commons.imaging.formats.tiff.constants.TiffTagConstants.*;
+
+/**
+ * Parses integer or floating-point scalar data from single-channel TIFF images, to double arrays.
+ *
+ * @apiNote this API has diverged from Terraplusplus, which has implementation specific to integers and floating-point images
+ * as a workaround for Apache commons imaging not initially supporting integer images.
+ *
+ * @author DaPorkchop_
+ * @author Smyler
+ */
+@JsonDeserialize
+public class ParseTiffDSP implements DoubleScalarParser {
+
+    @Override
+    @SneakyThrows(ImageReadException.class)
+    public double[] parse(int resolution, @NonNull ByteBuf buffer) throws IOException {
+        TiffContents contents = new TiffReader(false)
+                .readDirectories(new ByteSourceInputStream(new ByteBufInputStream(buffer), ""), true, FormatCompliance.getDefault());
+
+        double[] dst = new double[resolution * resolution];
+        TiffDirectory directory = this.parse(resolution, contents, dst);
+        this.postProcess(resolution, directory, dst);
+        return dst;
+    }
+
+    protected void postProcess(int resolution, @NonNull TiffDirectory directory, double @NonNull [] dst) throws ImageReadException, IOException {
+        TiffField nodataField = directory.findField(GdalLibraryTagConstants.EXIF_TAG_GDAL_NO_DATA);
+        if (nodataField != null) { //nodata value is set, replace all nodata values
+            double nodata = Double.parseDouble(nodataField.getStringValue());
+            for (int i = 0; i < dst.length; i++) {
+                if (dst[i] == nodata) {
+                    dst[i] = Double.NaN;
+                }
+            }
+        }
+    }
+
+    protected TiffDirectory parse(int resolution, @NonNull TiffContents contents, double @NonNull [] dst) throws ImageReadException, IOException {
+        for (TiffDirectory directory : contents.directories) {
+            if (this.parse(resolution, directory, dst)) {
+                return directory;
+            }
+        }
+        throw new IllegalArgumentException("no supported TIFF directories could be found!");
+    }
+
+    protected boolean parse(int resolution, @NonNull TiffDirectory directory, double @NonNull [] dst) throws ImageReadException, IOException {
+        if (!this.isSupportedFormat(directory)) {
+            return false;
+        }
+
+        TiffRasterData data = directory.getRasterData(null);
+        int w = data.getWidth();
+        int h = data.getHeight();
+        checkArg(w == resolution && h == resolution, "invalid image resolution: %dx%d (expected: %dx%3$d)", w, h, resolution);
+
+        //extend floats to doubles
+        float[] src = data.getData();
+        checkArg(src.length == dst.length, "data length invalid?!?");
+        for (int i = 0; i < resolution * resolution; i++) {
+            dst[i] = src[i];
+        }
+
+        return true;
+    }
+
+    /**
+     * Verify whether this is a supported format (single-channel grayscale, encoded as IEEE 754, integers, or signed integers).
+     *
+     * @param directory the TIFF directory to check
+     * @return true if the directory can be safely processed, false otherwise
+     *
+     * @throws ImageReadException if the directory is missing required fields
+     */
+    protected boolean isSupportedFormat(TiffDirectory directory) throws ImageReadException {
+        switch (directory.getFieldValue(TIFF_TAG_PHOTOMETRIC_INTERPRETATION)) {
+            case PHOTOMETRIC_INTERPRETATION_VALUE_WHITE_IS_ZERO:
+            case PHOTOMETRIC_INTERPRETATION_VALUE_BLACK_IS_ZERO:
+                break; //grayscale
+            default:
+                return false; //colored formats aren't supported lol
+        }
+        short[] bitsPerSample = directory.getFieldValue(TIFF_TAG_BITS_PER_SAMPLE, true);
+        if (bitsPerSample.length != 1) {
+            return false; //more than one channel
+        }
+        switch (directory.findField(TIFF_TAG_SAMPLE_FORMAT, true).getIntValue()) {
+            case SAMPLE_FORMAT_VALUE_UNSIGNED_INTEGER:
+            case SAMPLE_FORMAT_VALUE_TWOS_COMPLEMENT_SIGNED_INTEGER:
+            case SAMPLE_FORMAT_VALUE_IEEE_FLOATING_POINT:
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+}

src/main/java/net/buildtheearth/terraminusminus/config/scalarparse/d/ParseTiffFloatingPointDSP.java
@@ -1,19 +1,22 @@
 package net.buildtheearth.terraminusminus.config.scalarparse.d;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import lombok.NonNull;
-import org.apache.commons.imaging.ImageReadException;
-import org.apache.commons.imaging.formats.tiff.TiffDirectory;
+import net.buildtheearth.terraminusminus.TerraMinusMinus;
 
-import java.io.IOException;
 
 /**
- * @author DaPorkchop_
+ * Parses floating point scalar data from TIFF files.
+ *
+ * @deprecated floating point and integer specific TIFF implementations were made irrelevant
+ * by Apache commons imaging supporting them both ina unified way. Use {@link ParseTiffDSP} instead,
+ * this class may be removed in future releases.
  */
 @JsonDeserialize
-public class ParseTiffFloatingPointDSP extends ParseTiffAutoDSP {
-    @Override
-    protected boolean parseInteger(int resolution, @NonNull TiffDirectory directory, @NonNull double[] dst) throws ImageReadException, IOException {
-        return false;
+@Deprecated
+public class ParseTiffFloatingPointDSP extends ParseTiffDSP {
+
+    public ParseTiffFloatingPointDSP() {
+        TerraMinusMinus.LOGGER.warn("parse_tiff_fp and ParseTiffFloatingPointDSP are deprecated. Use parse_tiff and ParseTiffDSP instead");
     }
+
 }

src/main/java/net/buildtheearth/terraminusminus/config/scalarparse/d/ParseTiffIntDSP.java
@@ -1,19 +1,21 @@
 package net.buildtheearth.terraminusminus.config.scalarparse.d;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import lombok.NonNull;
-import org.apache.commons.imaging.ImageReadException;
-import org.apache.commons.imaging.formats.tiff.TiffDirectory;
-
-import java.io.IOException;
+import net.buildtheearth.terraminusminus.TerraMinusMinus;
 
 /**
- * @author DaPorkchop_
+ * Parses int scalar data from TIFF files.
+ *
+ * @deprecated floating point and integer specific TIFF implementations were made irrelevant
+ * by Apache commons imaging supporting them both ina unified way.
+ * Use {@link ParseTiffDSP} instead, this class may be removed in future releases.
  */
 @JsonDeserialize
+@Deprecated
 public class ParseTiffIntDSP extends ParseTiffAutoDSP {
-    @Override
-    protected boolean parseFloatingPoint(int resolution, @NonNull TiffDirectory directory, @NonNull double[] dst) throws ImageReadException, IOException {
-        return false;
+
+    public ParseTiffIntDSP() {
+        TerraMinusMinus.LOGGER.warn("parse_tiff_int and ParseTiffIntDSP are deprecated. Use parse_tiff and ParseTiffDSP instead");
     }
+
 }

BuildTheEarth/terraplusplus

(cherry picked from commit d41f73d8bf2daa87ffdddffc598d0138649d3c75)
# Conflicts:
#	README.md
#	build.gradle
#	src/main/java/net/buildtheearth/terraplusplus/config/GlobalParseRegistries.java
#	src/main/java/net/buildtheearth/terraplusplus/config/scalarparse/d/ParseTiffAutoDSP.java
#	src/main/java/net/buildtheearth/terraplusplus/config/scalarparse/d/ParseTiffDSP.java
#	src/main/resources/net/buildtheearth/terraplusplus/dataset/scalar/heights.json5

src/main/java/net/buildtheearth/terraplusplus/config/GlobalParseRegistries.java
@@ -20,6 +20,7 @@ import net.buildtheearth.terraplusplus.config.scalarparse.d.FlipZDSP;
 import net.buildtheearth.terraplusplus.config.scalarparse.d.FromIntDSP;
 import net.buildtheearth.terraplusplus.config.scalarparse.d.MultiplyDSP;
 import net.buildtheearth.terraplusplus.config.scalarparse.d.ParseTiffAutoDSP;
+import net.buildtheearth.terraplusplus.config.scalarparse.d.ParseTiffDSP;
 import net.buildtheearth.terraplusplus.config.scalarparse.d.ParseTiffFloatingPointDSP;
 import net.buildtheearth.terraplusplus.config.scalarparse.d.ParseTerrariumPngDSP;
 import net.buildtheearth.terraplusplus.config.scalarparse.d.ParseTiffIntDSP;
@@ -111,8 +112,9 @@ public class GlobalParseRegistries {
             .put("flip_z", FlipZDSP.class)
             .put("from_int", FromIntDSP.class)
             .put("swap_axes", SwapAxesDSP.class)
-            //parse operators
+            //parse operators (including deprecated ParseTiffXXXXDSPs)
             .put("parse_png_terrarium", ParseTerrariumPngDSP.class)
+            .put("parse_tiff", ParseTiffDSP.class)
             .put("parse_tiff_auto", ParseTiffAutoDSP.class)
             .put("parse_tiff_fp", ParseTiffFloatingPointDSP.class)
             .put("parse_tiff_int", ParseTiffIntDSP.class)

src/main/java/net/buildtheearth/terraplusplus/config/scalarparse/d/ParseTiffDSP.java
@@ -1,0 +1,112 @@
+package net.buildtheearth.terraplusplus.config.scalarparse.d;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import org.apache.commons.imaging.FormatCompliance;
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.common.bytesource.ByteSourceInputStream;
+import org.apache.commons.imaging.formats.tiff.*;
+import org.apache.commons.imaging.formats.tiff.constants.GdalLibraryTagConstants;
+
+import java.io.IOException;
+
+import static net.daporkchop.lib.common.util.PValidation.checkArg;
+import static org.apache.commons.imaging.formats.tiff.constants.TiffTagConstants.*;
+
+/**
+ * Parses integer or floating-point scalar data from single-channel TIFF images, to double arrays.
+ *
+ * @author DaPorkchop_
+ * @author Smyler
+ */
+@JsonDeserialize
+public class ParseTiffDSP implements DoubleScalarParser {
+
+    @Override
+    @SneakyThrows(ImageReadException.class)
+    public double[] parse(int resolution, @NonNull ByteBuf buffer) throws IOException {
+        TiffContents contents = new TiffReader(false)
+                .readDirectories(new ByteSourceInputStream(new ByteBufInputStream(buffer), ""), true, FormatCompliance.getDefault());
+
+        double[] dst = new double[resolution * resolution];
+        TiffDirectory directory = this.parse(resolution, contents, dst);
+        this.postProcess(resolution, directory, dst);
+        return dst;
+    }
+
+    protected void postProcess(int resolution, @NonNull TiffDirectory directory, double @NonNull [] dst) throws ImageReadException, IOException {
+        TiffField nodataField = directory.findField(GdalLibraryTagConstants.EXIF_TAG_GDAL_NO_DATA);
+        if (nodataField != null) { //nodata value is set, replace all nodata values
+            double nodata = Double.parseDouble(nodataField.getStringValue());
+            for (int i = 0; i < dst.length; i++) {
+                if (dst[i] == nodata) {
+                    dst[i] = Double.NaN;
+                }
+            }
+        }
+    }
+
+    protected TiffDirectory parse(int resolution, @NonNull TiffContents contents, double @NonNull [] dst) throws ImageReadException, IOException {
+        for (TiffDirectory directory : contents.directories) {
+            if (this.parse(resolution, directory, dst)) {
+                return directory;
+            }
+        }
+        throw new IllegalArgumentException("no supported TIFF directories could be found!");
+    }
+
+    protected boolean parse(int resolution, @NonNull TiffDirectory directory, double @NonNull [] dst) throws ImageReadException, IOException {
+        if (!this.isSupportedFormat(directory)) {
+            return false;
+        }
+
+        TiffRasterData data = directory.getRasterData(null);
+        int w = data.getWidth();
+        int h = data.getHeight();
+        checkArg(w == resolution && h == resolution, "invalid image resolution: %dx%d (expected: %dx%3$d)", w, h, resolution);
+
+        //extend floats to doubles
+        float[] src = data.getData();
+        checkArg(src.length == dst.length, "data length invalid?!?");
+        for (int i = 0; i < resolution * resolution; i++) {
+            dst[i] = src[i];
+        }
+
+        return true;
+    }
+
+    /**
+     * Verify whether this is a supported format (single-channel grayscale, encoded as IEEE 754, integers, or signed integers).
+     *
+     * @param directory the TIFF directory to check
+     * @return true if the directory can be safely processed, false otherwise
+     *
+     * @throws ImageReadException if the directory is missing required fields
+     */
+    protected boolean isSupportedFormat(TiffDirectory directory) throws ImageReadException {
+        switch (directory.getFieldValue(TIFF_TAG_PHOTOMETRIC_INTERPRETATION)) {
+            case PHOTOMETRIC_INTERPRETATION_VALUE_WHITE_IS_ZERO:
+            case PHOTOMETRIC_INTERPRETATION_VALUE_BLACK_IS_ZERO:
+                break; //grayscale
+            default:
+                return false; //colored formats aren't supported lol
+        }
+        short[] bitsPerSample = directory.getFieldValue(TIFF_TAG_BITS_PER_SAMPLE, true);
+        if (bitsPerSample.length != 1) {
+            return false; //more than one channel
+        }
+        switch (directory.findField(TIFF_TAG_SAMPLE_FORMAT, true).getIntValue()) {
+            case SAMPLE_FORMAT_VALUE_UNSIGNED_INTEGER:
+            case SAMPLE_FORMAT_VALUE_TWOS_COMPLEMENT_SIGNED_INTEGER:
+            case SAMPLE_FORMAT_VALUE_IEEE_FLOATING_POINT:
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+}

src/main/java/net/buildtheearth/terraplusplus/config/scalarparse/d/ParseTiffFloatingPointDSP.java
@@ -1,19 +1,22 @@
 package net.buildtheearth.terraplusplus.config.scalarparse.d;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import lombok.NonNull;
-import org.apache.commons.imaging.ImageReadException;
-import org.apache.commons.imaging.formats.tiff.TiffDirectory;
+import net.buildtheearth.terraplusplus.TerraMod;
 
-import java.io.IOException;
 
 /**
- * @author DaPorkchop_
+ * Parses floating point scalar data from TIFF files.
+ *
+ * @deprecated floating point and integer specific TIFF implementations were made irrelevant
+ * by Apache Commons imaging supporting them both ina unified way. Use {@link ParseTiffDSP} instead,
+ * this class may be removed in future releases.
  */
 @JsonDeserialize
-public class ParseTiffFloatingPointDSP extends ParseTiffAutoDSP {
-    @Override
-    protected boolean parseInteger(int resolution, @NonNull TiffDirectory directory, @NonNull double[] dst) throws ImageReadException, IOException {
-        return false;
+@Deprecated
+public class ParseTiffFloatingPointDSP extends ParseTiffDSP {
+
+    public ParseTiffFloatingPointDSP() {
+        TerraMod.LOGGER.warn("parse_tiff_fp and ParseTiffFloatingPointDSP are deprecated. Use parse_tiff and ParseTiffDSP instead");
     }
+
 }

src/main/java/net/buildtheearth/terraplusplus/config/scalarparse/d/ParseTiffIntDSP.java
@@ -1,19 +1,21 @@
 package net.buildtheearth.terraplusplus.config.scalarparse.d;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import lombok.NonNull;
-import org.apache.commons.imaging.ImageReadException;
-import org.apache.commons.imaging.formats.tiff.TiffDirectory;
-
-import java.io.IOException;
+import net.buildtheearth.terraplusplus.TerraMod;
 
 /**
- * @author DaPorkchop_
+ * Parses int scalar data from TIFF files.
+ *
+ * @deprecated floating point and integer specific TIFF implementations were made irrelevant
+ * by Apache Commons imaging supporting them both ina unified way.
+ * Use {@link ParseTiffDSP} instead, this class may be removed in future releases.
  */
 @JsonDeserialize
+@Deprecated
 public class ParseTiffIntDSP extends ParseTiffAutoDSP {
-    @Override
-    protected boolean parseFloatingPoint(int resolution, @NonNull TiffDirectory directory, @NonNull double[] dst) throws ImageReadException, IOException {
-        return false;
+
+    public ParseTiffIntDSP() {
+        TerraMod.LOGGER.warn("parse_tiff_int and ParseTiffIntDSP are deprecated. Use parse_tiff and ParseTiffDSP instead");
     }
+
 }